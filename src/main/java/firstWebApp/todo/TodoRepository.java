69655179b141b1c26daba6848546bf8617508bf3
pawelw89 <pawelwojciechowski89@wp.pl>
2020-10-11 11:04:27 +0200

    commit

---
+++ src/main/java/firstWebApp/todo/TodoRepository.java
@@ -0,0 +1,41 @@
+package firstWebApp.todo;
+
+import firstWebApp.HibernateUtil;
+
+import java.util.List;
+
+class TodoRepository {
+    List<Todo> findAll() {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.createQuery("from Todo", Todo.class).list();
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
+
+    Todo toggleTodo(Integer id) {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.get(Todo.class, id);
+        result.setDone(!result.isDone());
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
+
+    Todo addTodo(Todo newTodo) {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        session.persist(newTodo);
+
+        transaction.commit();
+        session.close();
+        return newTodo;
+    }
+}
